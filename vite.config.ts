2bd7c2b3fd259e9f96c5fe50268ff09569f49ef6
DeeqaJamalini <deeqa.j11@gmail.com>
2024-01-02 06:22:16 +0000

    deploying

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './punk-api-project/',
+  
   plugins: [react()],
+  base: '/punk-api-project/',
 })
