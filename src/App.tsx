06ee043d182528657f35d932cdaf26804298ade2
DeeqaJamalini <deeqa.j11@gmail.com>
2024-01-02 10:03:05 +0000

    tried to deploy again

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Main from "./components/Main/Main";
 import { Beer } from "./Types/Types";
 import BeerDetail from "./components/BeerDetail/BeerDetail";
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './punk-api-project/'
+  base: '/punk-api-project/'
   
   
 });
